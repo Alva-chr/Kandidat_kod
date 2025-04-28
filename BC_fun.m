9876c66e3ceedf5ffee58785583c87da051dd841
Alva-chr <alvachristensson03@gmail.com>
2025-04-28 11:04:57 +0200

    Tried but failed to fix issue

--- BC_fun.m
+++ BC_fun.m
@@ -3,9 +3,6 @@ function [L_w, L_e, L_s, L_n] = BC_fun(e1, e2, e3, e4, e5, m_x, m_y, e1_x, em_x,
 Im_x = speye(m_x);
 Im_y = speye(m_y);
 
-
-
-
 e_S = kron(e1_y', Im_x);  
 e_N = kron(em_y', Im_x);  
 e_W = kron(Im_y, e1_x');   
@@ -13,11 +10,11 @@ e_E = kron(Im_y, em_x');
 
 L_w = [ e_W*e1;
         e_W*e2 ];
-L_e = [ e_E*e3;
-        e_E*e4 ];
+L_e = [ e_E*e1;
+        e_E*e2 ];
 
 L_s = [ e_S*e1;
         e_S*e2 ];
-L_n = [ e_N*e1;
-        e_N*e2 ];
+L_n = [ e_N*e5;
+        e_N*e4 ];
 end
